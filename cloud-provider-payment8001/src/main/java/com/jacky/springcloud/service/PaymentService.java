c38e324307f80b32d426969336059b06bf5f3360
jackyjinchen <jackyjinchen@163.com>
2020-09-01 11:06:04 +0800

    新增payment8001

---
+++ cloud-provider-payment8001/src/main/java/com/jacky/springcloud/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.jacky.springcloud.service;
+
+import com.jacky.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+    public Payment getPaymentById(@Param("id") Long id);
+}
