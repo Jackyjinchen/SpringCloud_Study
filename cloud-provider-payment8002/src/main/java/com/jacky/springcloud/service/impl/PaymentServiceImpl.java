f31a717baabe0f63d5e114365cef69b8b040b328
jackyjinchen <jackyjinchen@163.com>
2020-09-03 09:31:59 +0800

    通过restTemplate@LoadBalance负载均衡

---
+++ cloud-provider-payment8002/src/main/java/com/jacky/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.jacky.springcloud.service.impl;
+
+import com.jacky.springcloud.dao.PaymentDao;
+import com.jacky.springcloud.entities.Payment;
+import com.jacky.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
