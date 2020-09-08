51acf47a99f37e3bc16047985d90704f3dca5c7e
jackyjinchen <jackyjinchen@163.com>
2020-09-08 11:40:26 +0800

    Stream整合RabbitMQ实现消息驱动、解决重复消费和持久化

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/jacky/springcloud/controller/ReceiveMessageController.java
@@ -0,0 +1,24 @@
+package com.jacky.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageController {
+    private MessageChannel input;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        System.out.println("消费者:"+message.getPayload()+"  port:"+serverPort);
+    }
+
+}
